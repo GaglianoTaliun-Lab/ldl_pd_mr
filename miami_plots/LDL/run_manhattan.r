6ba8d1e875ae12457bf4a4441a4a997d6285f45c
sgagliano <sgagliano@users.noreply.github.com>
2022-01-10 18:16:46 -0500

    Update run_manhattan.r

--- miami_plots/LDL/run_manhattan.r
+++ miami_plots/LDL/run_manhattan.r
@@ -1,4 +1,6 @@
 source('manhattan.plot-blue.R') #see https://github.com/GaglianoTaliun-Lab/useful_code_genetic_analysis/blob/main/Plotting/manhattan.plot-blue.R
+
+#female
 dd<-read.table("30780_irnt.gwas.imputed_v3.female.varorder.txt", h=T, as.is=T) #the summary statistics, three required columns: chromsosome, position and p-value
 png("LDL-FemaleManhattan.png", width=950, height=500)
 print(manhattan.plot(dd$CHR, dd$BP, dd$pval, sig.level=5e-8)) #where chr, pos and pvalue are the appropriate column headers in dd
